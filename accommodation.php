75c304d9c7ed72206ccc35fdeb654f980bae3649
twinklesahoo794 <103883052+sahootwinkle794@users.noreply.github.com>
2025-11-24 18:22:41 +0530

    accommodation page created

---
+++ accommodation.php
@@ -0,0 +1,160 @@
+ <?php include 'header-two.php'; ?>
+ 
+<!--================= Breadcrumb Area start =================-->
+      <section class="vs-breadcrumb" data-bg-src="home/img/bg/breadcrumb-bg-2.png">
+        <img src="home/img/icons/cloud.png" alt="vs-breadcrumb-icon" class="vs-breadcrumb-icon-1 animate-parachute" />
+        <img src="home/img/icons/ballon-sclation.png" alt="vs-breadcrumb-icon"
+          class="vs-breadcrumb-icon-2 animate-parachute" />
+        <div class="container">
+          <div class="row text-center">
+            <div class="col-12">
+              <div class="breadcrumb-content">
+                <h1 class="breadcrumb-title">Accomodation</h1>
+              </div>
+              <div class="breadcrumb-menu">
+                <ul class="custom-ul">
+                  <li>
+                    <a href="index.php">Home</a>
+                  </li>
+                  <li>Accomodation</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--================= Breadcrumb Area end =================-->
+ 
+ <!-- Rooms Three -->
+      <section class="rooms-three style-two">
+        <div class="auto-container">
+          <!-- Sec Title -->
+          <div class="sec-title title-anim">
+            <div class="sec-title_title">A PRIVATE SANCTUARY AWAITS YOU</div>
+            <h2 class="sec-title_heading title-anim">Accommodation</h2>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="room-block_two">
+                  <div class="room-block_two-inner card">
+                    <div class="room-block_two-image overlay-anim">
+                      <img class="trans-500" src="https://www.bellavistaresort.com/img/gallery/forest-wing.jpg"
+                        alt="" />
+                      <div class="room-block_two-overlay overlay-layer">
+                        <a class="book-btn theme-btn"
+                          href="https://bookings.resavenue.com/resBooking/availsearch?regCode=IGIA0303" target>Book
+                          Now</a>
+                      </div>
+                    </div>
+                    <div class="room-block_two-content">
+                      <!-- <div class="room-block_two-price">250$ <span>Night</span></div> -->
+                      <h3 class="room-block_two-title"><a href="javascript:void(0);">THE FOREST WING</a></h3>
+                      <p>The Forest Wing has the distinguished advantage of being a ‘drive-in’ zone, which practically
+                        enables you to drive your car to your door-step.</p>
+                      <!-- <div class="room-block_two-tag">Forest Deluxe (Ground Floor)</div>
+                      <div class="room-block_two-tag">Forest Duplex (1st Floor)</div> -->
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                 <div class="room-block_two">
+                  <div class="room-block_two-inner card">
+                    <div class="room-block_two-image overlay-anim">
+                      <img class="trans-500" src="https://www.bellavistaresort.com/img/gallery/redstone-wing.jpg"
+                        alt="" />
+                      <div class="room-block_two-overlay overlay-layer">
+                        <a class="book-btn theme-btn"
+                          href="https://bookings.resavenue.com/resBooking/availsearch?regCode=IGIA0303">Book Now</a>
+                      </div>
+                    </div>
+                    <div class="room-block_two-content">
+                      <!-- <div class="room-block_two-price">550$ <span>Night</span></div> -->
+                      <h3 class="room-block_two-title"><a href="javascript:void(0);">RED STONE WING</a></h3>
+                      <p>The Red stone Wing offers panoramic views of the valley or the town side. There are stairs to
+                        negotiate and the building does not have an elevator service.</p>
+                      <!-- <div class="room-block_two-tag">Single Bedroom</div> -->
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                 <div class="room-block_two">
+                  <div class="room-block_two-inner card">
+                    <div class="room-block_two-image overlay-anim">
+                      <img class="trans-500" src="assets/images/resource/orch-room.png" alt="" />
+                      <div class="room-block_two-overlay overlay-layer">
+                        <a class="book-btn theme-btn"
+                          href="https://bookings.resavenue.com/resBooking/availsearch?regCode=IGIA0303">Book Now</a>
+                      </div>
+                    </div>
+                    <div class="room-block_two-content">
+                      <!-- <div class="room-block_two-price">980$ <span>Night</span></div> -->
+                      <h3 class="room-block_two-title"><a href="javascript:void(0);">THE ORCHID WING</a></h3>
+                      <p>The freshly renovated Orchid Wing has modernized rooms offering scenic views of the valley or
+                        the backyard. Home of the suites, it boasts proximity to the parking area, children’s park, and
+                        swimming pool.</p>
+                      <!-- <div class="room-block_two-tag">Single Bedroom</div> -->
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="room-block_two">
+                  <div class="room-block_two-inner card">
+                    <div class="room-block_two-image overlay-anim">
+                      <img class="trans-500" src="https://www.bellavistaresort.com/img/gallery/forest-central-wing.jpg"
+                        alt="" />
+                      <div class="room-block_two-overlay overlay-layer">
+                        <a class="book-btn theme-btn"
+                          href="https://bookings.resavenue.com/resBooking/availsearch?regCode=IGIA0303">Book Now</a>
+                      </div>
+                    </div>
+                    <div class="room-block_two-content">
+                      <!-- <div class="room-block_two-price">980$ <span>Night</span></div> -->
+                      <h3 class="room-block_two-title"><a href="javascript:void(0);">FOREST CENTRAL WING</a></h3>
+                      <p>Well appointed rooms overlooking the Forest Cafe with an option to choose from 'One Queen bed'
+                        layout for a couple or 'Two Queen beds' room suitable for a family. Situated in the drive-in
+                        zone of the Forest Wing for easy accessibility.</p>
+                      <!-- <div class="room-block_two-tag">Single Bedroom</div> -->
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                 <div class="room-block_two">
+                  <div class="room-block_two-inner card">
+                    <div class="room-block_two-image overlay-anim">
+                      <img class="trans-500"
+                        src="https://www.bellavistaresort.com/images/forest-executive-room/forest-executive-room-05.jpg"
+                        alt="" />
+                      <div class="room-block_two-overlay overlay-layer">
+                        <a class="book-btn theme-btn"
+                          href="https://bookings.resavenue.com/resBooking/availsearch?regCode=IGIA0303">Book Now</a>
+                      </div>
+                    </div>
+                    <div class="room-block_two-content">
+                      <!-- <div class="room-block_two-price">980$ <span>Night</span></div> -->
+                      <h3 class="room-block_two-title"><a href="javascript:void(0);">APARTMENT WING</a></h3>
+                      <p>Enjoy the privacy of your own private space with a layout of 2 private bedrooms with balconies
+                        and a living room apartment. The apartment on the first floor has an additional vestibule area.
+                      </p>
+                      <!-- <div class="room-block_two-tag">Single Bedroom</div> -->
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+      <!-- End Rooms Three -->
+
+<?php include 'footer-two.php'; ?>
